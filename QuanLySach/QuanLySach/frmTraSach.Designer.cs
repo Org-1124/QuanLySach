055bfda3f0d45b67511348a0635b52774b196b4e
lehaisonmath6 <lehaisonmath6@gmail.com>
2016-11-23 19:51:22 +0700

    ok men

--- QuanLySach/QuanLySach/frmTraSach.Designer.cs
+++ QuanLySach/QuanLySach/frmTraSach.Designer.cs
@@ -30,38 +30,42 @@
         {
             this.components = new System.ComponentModel.Container();
             this.pn = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.rdbTenDocGia = new System.Windows.Forms.RadioButton();
+            this.rdbTenSach = new System.Windows.Forms.RadioButton();
+            this.btnHoanTat = new System.Windows.Forms.Button();
+            this.rdbIDDocGia = new System.Windows.Forms.RadioButton();
+            this.rdbIDSach = new System.Windows.Forms.RadioButton();
             this.btnTimKiem = new System.Windows.Forms.Button();
-            this.rdbAnBan = new System.Windows.Forms.RadioButton();
-            this.rdbDocGia = new System.Windows.Forms.RadioButton();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.lsvAnBan = new System.Windows.Forms.ListView();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.dgvAnBan = new System.Windows.Forms.DataGridView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.trảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.trảToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnHoanTat = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lsvAnBan = new System.Windows.Forms.ListView();
             this.colIDAnBan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colTenSach = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colIDThe = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.huyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pn.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnBan)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pn
             // 
+            this.pn.Controls.Add(this.rdbTenDocGia);
+            this.pn.Controls.Add(this.rdbTenSach);
             this.pn.Controls.Add(this.btnHoanTat);
-            this.pn.Controls.Add(this.rdbDocGia);
-            this.pn.Controls.Add(this.rdbAnBan);
+            this.pn.Controls.Add(this.rdbIDDocGia);
+            this.pn.Controls.Add(this.rdbIDSach);
             this.pn.Controls.Add(this.btnTimKiem);
             this.pn.Controls.Add(this.txtTimKiem);
             this.pn.Controls.Add(this.label1);
@@ -70,6 +74,86 @@
             this.pn.Name = "pn";
             this.pn.Size = new System.Drawing.Size(1055, 77);
             this.pn.TabIndex = 0;
+            // 
+            // rdbTenDocGia
+            // 
+            this.rdbTenDocGia.AutoSize = true;
+            this.rdbTenDocGia.Location = new System.Drawing.Point(331, 54);
+            this.rdbTenDocGia.Name = "rdbTenDocGia";
+            this.rdbTenDocGia.Size = new System.Drawing.Size(83, 17);
+            this.rdbTenDocGia.TabIndex = 7;
+            this.rdbTenDocGia.TabStop = true;
+            this.rdbTenDocGia.Text = "Tên độc giả";
+            this.rdbTenDocGia.UseVisualStyleBackColor = true;
+            // 
+            // rdbTenSach
+            // 
+            this.rdbTenSach.AutoSize = true;
+            this.rdbTenSach.Location = new System.Drawing.Point(246, 54);
+            this.rdbTenSach.Name = "rdbTenSach";
+            this.rdbTenSach.Size = new System.Drawing.Size(70, 17);
+            this.rdbTenSach.TabIndex = 6;
+            this.rdbTenSach.TabStop = true;
+            this.rdbTenSach.Text = "Tên sách";
+            this.rdbTenSach.UseVisualStyleBackColor = true;
+            // 
+            // btnHoanTat
+            // 
+            this.btnHoanTat.Location = new System.Drawing.Point(639, 19);
+            this.btnHoanTat.Name = "btnHoanTat";
+            this.btnHoanTat.Size = new System.Drawing.Size(75, 23);
+            this.btnHoanTat.TabIndex = 5;
+            this.btnHoanTat.Text = "Hoàn tất";
+            this.btnHoanTat.UseVisualStyleBackColor = true;
+            this.btnHoanTat.Click += new System.EventHandler(this.btnHoanTat_Click);
+            // 
+            // rdbIDDocGia
+            // 
+            this.rdbIDDocGia.AutoSize = true;
+            this.rdbIDDocGia.Location = new System.Drawing.Point(153, 54);
+            this.rdbIDDocGia.Name = "rdbIDDocGia";
+            this.rdbIDDocGia.Size = new System.Drawing.Size(76, 17);
+            this.rdbIDDocGia.TabIndex = 4;
+            this.rdbIDDocGia.TabStop = true;
+            this.rdbIDDocGia.Text = "ID Độc giả";
+            this.rdbIDDocGia.UseVisualStyleBackColor = true;
+            // 
+            // rdbIDSach
+            // 
+            this.rdbIDSach.AutoSize = true;
+            this.rdbIDSach.Location = new System.Drawing.Point(79, 54);
+            this.rdbIDSach.Name = "rdbIDSach";
+            this.rdbIDSach.Size = new System.Drawing.Size(64, 17);
+            this.rdbIDSach.TabIndex = 3;
+            this.rdbIDSach.TabStop = true;
+            this.rdbIDSach.Text = "ID Sách";
+            this.rdbIDSach.UseVisualStyleBackColor = true;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(429, 19);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(79, 21);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(335, 20);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm kiếm";
             // 
             // panel2
             // 
@@ -80,78 +164,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1055, 348);
             this.panel2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(142, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tìm kiếm";
-            // 
-            // txtTimKiem
-            // 
-            this.txtTimKiem.Location = new System.Drawing.Point(216, 20);
-            this.txtTimKiem.Name = "txtTimKiem";
-            this.txtTimKiem.Size = new System.Drawing.Size(187, 20);
-            this.txtTimKiem.TabIndex = 1;
-            // 
-            // btnTimKiem
-            // 
-            this.btnTimKiem.Location = new System.Drawing.Point(426, 18);
-            this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
-            this.btnTimKiem.TabIndex = 2;
-            this.btnTimKiem.Text = "Tìm kiếm";
-            this.btnTimKiem.UseVisualStyleBackColor = true;
-            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
-            // 
-            // rdbAnBan
-            // 
-            this.rdbAnBan.AutoSize = true;
-            this.rdbAnBan.Location = new System.Drawing.Point(216, 54);
-            this.rdbAnBan.Name = "rdbAnBan";
-            this.rdbAnBan.Size = new System.Drawing.Size(59, 17);
-            this.rdbAnBan.TabIndex = 3;
-            this.rdbAnBan.TabStop = true;
-            this.rdbAnBan.Text = "Ấn bản";
-            this.rdbAnBan.UseVisualStyleBackColor = true;
-            // 
-            // rdbDocGia
-            // 
-            this.rdbDocGia.AutoSize = true;
-            this.rdbDocGia.Location = new System.Drawing.Point(341, 54);
-            this.rdbDocGia.Name = "rdbDocGia";
-            this.rdbDocGia.Size = new System.Drawing.Size(62, 17);
-            this.rdbDocGia.TabIndex = 4;
-            this.rdbDocGia.TabStop = true;
-            this.rdbDocGia.Text = "Độc giả";
-            this.rdbDocGia.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.lsvAnBan);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(788, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(267, 348);
-            this.panel3.TabIndex = 0;
-            // 
-            // lsvAnBan
-            // 
-            this.lsvAnBan.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.colIDAnBan,
-            this.colTenSach,
-            this.colIDThe});
-            this.lsvAnBan.ContextMenuStrip = this.contextMenuStrip2;
-            this.lsvAnBan.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lsvAnBan.Location = new System.Drawing.Point(0, 0);
-            this.lsvAnBan.Name = "lsvAnBan";
-            this.lsvAnBan.Size = new System.Drawing.Size(267, 348);
-            this.lsvAnBan.TabIndex = 0;
-            this.lsvAnBan.UseCompatibleStateImageBehavior = false;
-            this.lsvAnBan.View = System.Windows.Forms.View.Details;
             // 
             // panel4
             // 
@@ -164,6 +176,7 @@
             // 
             // dgvAnBan
             // 
+            this.dgvAnBan.AllowUserToResizeRows = false;
             this.dgvAnBan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvAnBan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAnBan.ContextMenuStrip = this.contextMenuStrip1;
@@ -185,20 +198,6 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(91, 26);
             // 
-            // contextMenuStrip2
-            // 
-            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.trảToolStripMenuItem});
-            this.contextMenuStrip2.Name = "contextMenuStrip2";
-            this.contextMenuStrip2.Size = new System.Drawing.Size(97, 26);
-            // 
-            // trảToolStripMenuItem
-            // 
-            this.trảToolStripMenuItem.Name = "trảToolStripMenuItem";
-            this.trảToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.trảToolStripMenuItem.Text = "Hủy";
-            this.trảToolStripMenuItem.Click += new System.EventHandler(this.trảToolStripMenuItem_Click);
-            // 
             // trảToolStripMenuItem1
             // 
             this.trảToolStripMenuItem1.Name = "trảToolStripMenuItem1";
@@ -206,15 +205,33 @@
             this.trảToolStripMenuItem1.Text = "Trả";
             this.trảToolStripMenuItem1.Click += new System.EventHandler(this.trảToolStripMenuItem1_Click);
             // 
-            // btnHoanTat
+            // panel3
             // 
-            this.btnHoanTat.Location = new System.Drawing.Point(583, 18);
-            this.btnHoanTat.Name = "btnHoanTat";
-            this.btnHoanTat.Size = new System.Drawing.Size(75, 23);
-            this.btnHoanTat.TabIndex = 5;
-            this.btnHoanTat.Text = "Hoàn tất";
-            this.btnHoanTat.UseVisualStyleBackColor = true;
-            this.btnHoanTat.Click += new System.EventHandler(this.btnHoanTat_Click);
+            this.panel3.Controls.Add(this.lsvAnBan);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(788, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(267, 348);
+            this.panel3.TabIndex = 0;
+            // 
+            // lsvAnBan
+            // 
+            this.lsvAnBan.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colIDAnBan,
+            this.colTenSach,
+            this.colIDThe});
+            this.lsvAnBan.ContextMenuStrip = this.contextMenuStrip2;
+            this.lsvAnBan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvAnBan.FullRowSelect = true;
+            this.lsvAnBan.GridLines = true;
+            this.lsvAnBan.HideSelection = false;
+            this.lsvAnBan.Location = new System.Drawing.Point(0, 0);
+            this.lsvAnBan.MultiSelect = false;
+            this.lsvAnBan.Name = "lsvAnBan";
+            this.lsvAnBan.Size = new System.Drawing.Size(267, 348);
+            this.lsvAnBan.TabIndex = 0;
+            this.lsvAnBan.UseCompatibleStateImageBehavior = false;
+            this.lsvAnBan.View = System.Windows.Forms.View.Details;
             // 
             // colIDAnBan
             // 
@@ -230,6 +247,20 @@
             this.colIDThe.Text = "Mã Độc Giả";
             this.colIDThe.Width = 78;
             // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.huyToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(97, 26);
+            // 
+            // huyToolStripMenuItem
+            // 
+            this.huyToolStripMenuItem.Name = "huyToolStripMenuItem";
+            this.huyToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.huyToolStripMenuItem.Text = "Hủy";
+            this.huyToolStripMenuItem.Click += new System.EventHandler(this.huyToolStripMenuItem_Click);
+            // 
             // frmTraSach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -239,13 +270,14 @@
             this.Controls.Add(this.pn);
             this.Name = "frmTraSach";
             this.Text = "frmTraSach";
+            this.Load += new System.EventHandler(this.frmTraSach_Load);
             this.pn.ResumeLayout(false);
             this.pn.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnBan)).EndInit();
             this.contextMenuStrip1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -254,8 +286,8 @@
         #endregion
 
         private System.Windows.Forms.Panel pn;
-        private System.Windows.Forms.RadioButton rdbDocGia;
-        private System.Windows.Forms.RadioButton rdbAnBan;
+        private System.Windows.Forms.RadioButton rdbIDDocGia;
+        private System.Windows.Forms.RadioButton rdbIDSach;
         private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.TextBox txtTimKiem;
         private System.Windows.Forms.Label label1;
@@ -267,10 +299,12 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem trảToolStripMenuItem1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
-        private System.Windows.Forms.ToolStripMenuItem trảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem huyToolStripMenuItem;
         private System.Windows.Forms.Button btnHoanTat;
         private System.Windows.Forms.ColumnHeader colIDAnBan;
         private System.Windows.Forms.ColumnHeader colTenSach;
         private System.Windows.Forms.ColumnHeader colIDThe;
+        private System.Windows.Forms.RadioButton rdbTenDocGia;
+        private System.Windows.Forms.RadioButton rdbTenSach;
     }
 }
--- QuanLySach/QuanLySach/frmTraSach.cs
+++ QuanLySach/QuanLySach/frmTraSach.cs
@@ -17,6 +17,7 @@ namespace QuanLySach
         {
             InitializeComponent();
         }
+        
         enum LuaChon
         {
             AnBan,
@@ -25,16 +26,19 @@ namespace QuanLySach
         LuaChon lc;
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
-            if (!rdbAnBan.Checked && !rdbDocGia.Checked)
-            {
-                MessageBox.Show("Yêu cầu bạn chọn thể loại");
-            }
-            if (rdbAnBan.Checked)
+            if (rdbIDSach.Checked)
             {
                 lc = LuaChon.AnBan;
-                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.tblDauSach.TenSach.Contains(txtTimKiem.Text) && n.TrangThai == 0).ToList();
+                int idanban = 0;
+                int.TryParse(txtTimKiem.Text,out idanban);
+                if(idanban==0)
+                {
+                    MessageBox.Show("Nhập mã ấn bản");
+                    return;
+                }
+                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.TrangThai == 0&&n.IDSach==idanban).Select(n => new ViewTraSach { IDAnBan = n.IDAnBan, IDSach = (int)n.IDSach, TenSach = n.tblDauSach.TenSach, TrangThai = n.TrangThai == 0 ? "Đang mượn" : "Có sẵn", IDDocGia = n.tblPhieuMuon.IDThe, TenDocGia = n.tblPhieuMuon.tblDocGia.HoTen }).ToList();
             }
-            else
+            else if(rdbIDDocGia.Checked)
             {
                 int iddocgia=0;
                 int.TryParse(txtTimKiem.Text,out iddocgia);
@@ -44,18 +48,26 @@ namespace QuanLySach
                     return;
                 }
                 lc = LuaChon.DocGia;
-                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.tblPhieuMuon.IDThe == iddocgia).ToList();
+                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.TrangThai == 0 && n.tblPhieuMuon.IDThe==iddocgia).Select(n => new ViewTraSach { IDAnBan = n.IDAnBan, IDSach = (int)n.IDSach, TenSach = n.tblDauSach.TenSach, TrangThai = n.TrangThai == 0 ? "Đang mượn" : "Có sẵn", IDDocGia = n.tblPhieuMuon.IDThe, TenDocGia = n.tblPhieuMuon.tblDocGia.HoTen }).ToList();
             }
-            dgvAnBan.Columns["tblDauSach"].Visible = false;
-            dgvAnBan.Columns["tblPhieuMuon"].Visible = false;
-            dgvAnBan.Columns["tblPhieuTra"].Visible = false;
-            dgvAnBan.Columns["IDPhieuTra"].Visible = false;
+            else if(rdbTenDocGia.Checked)
+            {
+                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.TrangThai == 0 && n.tblPhieuMuon.tblDocGia.HoTen.Contains(txtTimKiem.Text)).Select(n => new ViewTraSach { IDAnBan = n.IDAnBan, IDSach = (int)n.IDSach, TenSach = n.tblDauSach.TenSach, TrangThai = n.TrangThai == 0 ? "Đang mượn" : "Có sẵn", IDDocGia = n.tblPhieuMuon.IDThe, TenDocGia = n.tblPhieuMuon.tblDocGia.HoTen }).ToList();
+            }
+            else if(rdbTenSach.Checked)
+            {
+                dgvAnBan.DataSource = database.tblAnBans.Where(n => n.TrangThai == 0 && n.tblDauSach.TenSach.Contains(txtTimKiem.Text)).Select(n => new ViewTraSach { IDAnBan = n.IDAnBan, IDSach = (int)n.IDSach, TenSach = n.tblDauSach.TenSach, TrangThai = n.TrangThai == 0 ? "Đang mượn" : "Có sẵn", IDDocGia = n.tblPhieuMuon.IDThe, TenDocGia = n.tblPhieuMuon.tblDocGia.HoTen }).ToList();
+            }
+            else
+            {
+                LoadDuLieu();
+            }
         }
-        tblAnBan anbanselected;
+        ViewTraSach anbanselected;
         private void dgvAnBan_Click(object sender, EventArgs e)
         {
             DataGridViewRow row = dgvAnBan.SelectedRows[0];
-            anbanselected = (tblAnBan)row.DataBoundItem;
+            anbanselected = (ViewTraSach)row.DataBoundItem;
         }
 
         private void trảToolStripMenuItem1_Click(object sender, EventArgs e)
@@ -65,38 +77,69 @@ namespace QuanLySach
                 MessageBox.Show("Mời bạn chọn ấn bản");
                 return;
             }
+            tblAnBan ab = database.tblAnBans.SingleOrDefault(n => n.IDAnBan == anbanselected.IDAnBan);
+            ab.TrangThai = 1;
+            tblDauSach s = database.tblDauSaches.SingleOrDefault(n => n.IDSach == anbanselected.IDSach);
+            s.SoLuongConLai++;
+            database.SaveChanges();
+            btnTimKiem_Click(null, null);
             ListViewItem item = new ListViewItem(anbanselected.IDAnBan.ToString());
-            item.SubItems.Add(anbanselected.tblDauSach.TenSach);
-            item.SubItems.Add(anbanselected.tblPhieuMuon.IDThe.ToString());
+            item.SubItems.Add(anbanselected.TenSach);
+            item.SubItems.Add(anbanselected.IDDocGia.ToString());
             lsvAnBan.Items.Add(item);
+            anbanselected = null;
         }
 
-        private void trảToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnHoanTat_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đã trả thành công");
+        }
+        class ViewTraSach
+        {
+            public int IDAnBan { get; set; }
+            public int IDSach { get; set; }
+            
+            public string TenSach { get; set; }
+            public string TrangThai { get; set; }
+           
+            public int IDDocGia { get; set; }
+            public string TenDocGia { get; set; }
+           
+        }
+        private void frmTraSach_Load(object sender, EventArgs e)
+        {
+            LoadDuLieu();  
+        }
+        void LoadDuLieu()
+        {
+            List<ViewTraSach> lst = database.tblAnBans.Where(n => n.TrangThai == 0).Select(n => new ViewTraSach { IDAnBan = n.IDAnBan, IDSach = (int)n.IDSach, TenSach = n.tblDauSach.TenSach, TrangThai = n.TrangThai == 0 ? "Đang mượn" : "Có sẵn", IDDocGia = n.tblPhieuMuon.IDThe, TenDocGia = n.tblPhieuMuon.tblDocGia.HoTen }).ToList();
+            dgvAnBan.DataSource = lst;
+            dgvAnBan.Columns["IDAnBan"].HeaderText = "Mã ấn bản";
+            dgvAnBan.Columns["IDSach"].HeaderText = "Mã sách";
+            dgvAnBan.Columns["TenSach"].HeaderText = "Tên sách";
+            dgvAnBan.Columns["TrangThai"].HeaderText = "Trạng thái";
+            dgvAnBan.Columns["IDDocGia"].HeaderText = "Mã độc giả";
+            dgvAnBan.Columns["TenDocGia"].HeaderText = "Tên độc giả";
+        }
+
+        private void huyToolStripMenuItem_Click(object sender, EventArgs e)
         {
             try
             {
                 ListViewItem item = lsvAnBan.SelectedItems[0];
                 lsvAnBan.Items.Remove(item);
+                int idab = int.Parse(item.SubItems[0].Text);
+                tblAnBan ab = database.tblAnBans.SingleOrDefault(n => n.IDAnBan == idab);
+                ab.TrangThai = 0;
+                tblDauSach s = database.tblDauSaches.SingleOrDefault(n => n.IDSach == anbanselected.IDSach);
+                s.SoLuongConLai--;
+                database.SaveChanges();
+                btnTimKiem_Click(null, null);
             }
             catch
             {
                 MessageBox.Show("Yêu cầu bạn chọn sách cần chọn");
             }
-          
-        }
-
-        private void btnHoanTat_Click(object sender, EventArgs e)
-        {
-            foreach(ListViewItem item in lsvAnBan.Items)
-            {
-                int idanban = int.Parse(item.SubItems[0].Text);
-                tblAnBan ab = database.tblAnBans.SingleOrDefault(n => n.IDAnBan == idanban);
-                ab.TrangThai = 1;
-                ab.tblPhieuMuon = null;
-            }
-            database.SaveChanges();
-            MessageBox.Show("Đã trả thành công");
-            this.Close();
         }
     }
 }
